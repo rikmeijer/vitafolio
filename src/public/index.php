813c56182a0f0a2cd1245207af2c903638dfcb66
Rik Meijer <rikmeijer@gmail.com>
2014-11-20 20:32:10 +0100

    replace static html with PHP HTML5 Elements

--- src/public/index.php
+++ src/public/index.php
@@ -1,1 +1,12 @@
-<html><head></head><body>Hello World!</body></html>
+<?php 
+$bootstrap = require dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'bootstrap.php';
+$bootstrap();
+
+$element = HTML5\Element::withChildren('html', array(
+    new HTML5\Element('head'),
+    HTML5\Element::withChildren('body', array(
+        new HTML5\Text('Hello World!')
+    ))
+));
+
+print $element->build();
