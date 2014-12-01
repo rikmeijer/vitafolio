341b0c43cf4e7b956d9ee449159be4f637324730
Rik Meijer <rikmeijer@gmail.com>
2014-12-01 21:49:27 +0100

    fix incomplete tests

--- test/library/HTML5/FactoryTest.php
+++ test/library/HTML5/FactoryTest.php
@@ -13,7 +13,6 @@ class FactoryTest extends \PHPUnit_Framework_TestCase
         $factory = new Factory(array());
         
         $document = $factory->createDocumentWithChildren(array(new Node\Element('div')));
-        $this->assertTrue($document instanceof Document);
         $this->assertEquals('<!DOCTYPE html>' . PHP_EOL . '<html><head><body><div></body></html>', $document->build());
     }
     
@@ -21,12 +20,12 @@ class FactoryTest extends \PHPUnit_Framework_TestCase
     public function testCreateElement()
     {
         $factory = new Factory(array());
-        $this->assertTrue($factory->createElement('html') instanceof Node\Element);
+        $this->assertEquals('<html>', $factory->createElement('html')->build());
     }
     
     public function testCreateText()
     {
         $factory = new Factory(array());
-        $this->assertTrue($factory->createText('html') instanceof Node\Text);
+        $this->assertEquals('html', $factory->createText('html')->build());
     }
 }
