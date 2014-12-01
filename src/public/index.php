e85212e5ecc1ebe35f5a9f99fa0695dc089e027a
Rik Meijer <rikmeijer@gmail.com>
2014-12-01 21:43:53 +0100

    remove duplication

--- src/public/index.php
+++ src/public/index.php
@@ -1,7 +1,8 @@
 <?php
 return function(array $services)  {
-    $document = $services['library']['HTML5']()->createDocumentWithChildren(array(
-        $services['library']['HTML5']()->createText('Hello World!')
+    $html5Factory = $services['library']['HTML5'](); 
+    $document = $html5Factory->createDocumentWithChildren(array(
+        $html5Factory->createText('Hello World!')
     ));
         
     return $services['filewriter'](DIRECTORY_SEPARATOR . 'index.html', $document->build());
