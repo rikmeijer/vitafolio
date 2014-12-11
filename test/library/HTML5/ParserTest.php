64c587ff4f7cfab46eb84c6dc4d8cd24f8c5de57
Rik Meijer <rikmeijer@gmail.com>
2014-12-11 21:22:50 +0100

    included debug information

--- test/library/HTML5/ParserTest.php
+++ test/library/HTML5/ParserTest.php
@@ -8,7 +8,7 @@ class ParserTest extends \PHPUnit_Framework_TestCase
     {
         $parser = new Parser(new Factory(array()));
         $result = $parser->parse('<element></element>');
-        $this->assertEquals('<element></element>', $result[0]->build());
+        $this->assertEquals('<element></element>', $result[0]->build(), '<element></element>');
     }
     
     public function testParseDocument()
@@ -16,16 +16,16 @@ class ParserTest extends \PHPUnit_Framework_TestCase
         $parser = new Parser(new Factory(array()));
         $result = $parser->parse('<!DOCTYPE html><html><head></head><body><element></element></body></html>');
         $this->assertTrue($result instanceof Document);
-        $this->assertEquals('<!DOCTYPE html>' . PHP_EOL .'<html><head></head><body><element></element></body></html>', $result->build());
+        $this->assertEquals('<!DOCTYPE html>' . PHP_EOL .'<html><head></head><body><element></element></body></html>', $result->build(), '<!DOCTYPE html><html><head></head><body><element></element></body></html>');
     }
     
     
     public function testParseDocumentMultipleRoot()
     {
         $parser = new Parser(new Factory(array()));
-        $result = $parser->parse('<!DOCTYPE html><html2></html2><html3></htm3l>');
+        $result = $parser->parse('<!DOCTYPE html><html2></html2><html3></html3>');
         $this->assertTrue($result instanceof Document);
-        $this->assertEquals('<!DOCTYPE html>' . PHP_EOL .'<html><head><body></html>', $result->build());
+        $this->assertEquals('<!DOCTYPE html>' . PHP_EOL .'<html><head><body></html>', $result->build(), '<!DOCTYPE html><html2></html2><html3></html3>');
     }
     
     public function testParseDocumentNoRoot()
@@ -33,7 +33,7 @@ class ParserTest extends \PHPUnit_Framework_TestCase
         $parser = new Parser(new Factory(array()));
         $result = $parser->parse('<!DOCTYPE html> ');
         $this->assertTrue($result instanceof Document);
-        $this->assertEquals('<!DOCTYPE html>' . PHP_EOL .'<html><head><body></html>', $result->build());
+        $this->assertEquals('<!DOCTYPE html>' . PHP_EOL .'<html><head><body></html>', $result->build(), '<!DOCTYPE html> ');
     }
     
     public function testParseDocumentInvalidRoot()
@@ -41,41 +41,41 @@ class ParserTest extends \PHPUnit_Framework_TestCase
         $parser = new Parser(new Factory(array()));
         $result = $parser->parse('<!DOCTYPE html><html2></html2>');
         $this->assertTrue($result instanceof Document);
-        $this->assertEquals('<!DOCTYPE html>' . PHP_EOL .'<html><head><body></html>', $result->build());
+        $this->assertEquals('<!DOCTYPE html>' . PHP_EOL .'<html><head><body></html>', $result->build(), '<!DOCTYPE html><html2></html2>');
     }
 
     public function testParseTagName()
     {
         $parser = new Parser(new Factory(array()));
         $result = $parser->parse('<element2></element2>');
-        $this->assertEquals('<element2></element2>', $result[0]->build());
+        $this->assertEquals('<element2></element2>', $result[0]->build(), '<element2></element2>');
     }
 
     public function testParseClosedEmptyElement()
     {
         $parser = new Parser(new Factory(array()));
         $result = $parser->parse('<element2 />');
-        $this->assertEquals('<element2>', $result[0]->build());
+        $this->assertEquals('<element2>', $result[0]->build(), '<element2 />');
     }
 
     public function testParseChildren()
     {
         $parser = new Parser(new Factory(array()));
         $result = $parser->parse('<element2><child></child><element2 attr1="val1" attr2=val2 attr3="val 3"></element2></element2>');
-        $this->assertEquals('<element2><child></child><element2 attr1="val1" attr2="val2" attr3="val 3"></element2></element2>', $result[0]->build());
+        $this->assertEquals('<element2><child></child><element2 attr1="val1" attr2="val2" attr3="val 3"></element2></element2>', $result[0]->build(), '<element2><child></child><element2 attr1="val1" attr2=val2 attr3="val 3"></element2></element2>');
     }
     
     public function testParseWithAttribute()
     {
         $parser = new Parser(new Factory(array()));
         $result = $parser->parse('<element attr1="val1"></element>');
-        $this->assertEquals('<element attr1="val1"></element>', $result[0]->build());
+        $this->assertEquals('<element attr1="val1"></element>', $result[0]->build(), '<element attr1="val1"></element>');
     }
     
     public function testParseWithAttributes()
     {
         $parser = new Parser(new Factory(array()));
         $result = $parser->parse('<element attr1="val1" attr2=val2 attr3="val 3"></element>');
-        $this->assertEquals('<element attr1="val1" attr2="val2" attr3="val 3"></element>', $result[0]->build());
+        $this->assertEquals('<element attr1="val1" attr2="val2" attr3="val 3"></element>', $result[0]->build(), '<element attr1="val1" attr2=val2 attr3="val 3"></element>');
     }
 }
