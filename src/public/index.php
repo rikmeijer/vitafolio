1d0c6127efb3ad03d63dfaddd62abbbbd0c730a1
Rik Meijer <rikmeijer@gmail.com>
2014-12-11 21:23:19 +0100

    use template for index.php

--- src/public/index.php
+++ src/public/index.php
@@ -1,9 +1,7 @@
 <?php
 return function(array $services)  {
     $html5Factory = $services['library']['HTML5'](); 
-    $document = $html5Factory->createDocumentWithChildren(array(
-        $html5Factory->createText('Hello World!')
-    ));
+    $document = $html5Factory->createParser()->parse($services['template']('default.html'));
     
     $document->setStyles(array(
        'font-family' => 'Arial, sans-serif' 
