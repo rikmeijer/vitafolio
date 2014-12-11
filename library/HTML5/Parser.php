a71ed0ae6fb19abdfac7743179f9cc64224d14ea
Rik Meijer <rikmeijer@gmail.com>
2014-12-11 21:23:04 +0100

    rewrote parser

--- library/HTML5/Parser.php
+++ library/HTML5/Parser.php
@@ -24,44 +24,120 @@ class Parser
         $elements = array();
         if (strpos($string, Document::DOCTYPE) === 0) {
             $document = $this->factory->createDocument();
-            
-            $children = $this->parse(substr($string, strlen(Document::DOCTYPE)));
+            $children = $this->parse(trim(substr($string, strlen(Document::DOCTYPE)))); //skip newline
             if (count($children) > 1) {
                 return $document;
-            }
-            
-            if (!$children[0] instanceof Node\Element) {
+            } elseif (!$children[0] instanceof Node\Element) {
                 return $document;
             } elseif ($children[0]->getName() !== 'html') {
                 return $document;
             }
-
             $document->addChild($children[0]);
-            
             return $document;
-            
-        } elseif (preg_match_all('/\<(?<name>\w+)(?<attributes>(\s+\w+\=(\w+|"[^"]+"))*)(\s+\/>|>((?<innerHTML>.*))<\/\1>)?/', $string, $matches, PREG_SET_ORDER) < 1) {
-            return array($this->factory->createText($string));
         }
         
-        foreach ($matches as $match) {
-            $element = $this->factory->createElement($match['name']);
-            
-            if (array_key_exists('attributes', $match)) {
-                preg_match_all('/(?<identifier>\w+)\=(?<value>(\w+|"[^"]+"))/', $match['attributes'], $attributeMatches, PREG_SET_ORDER);
-                foreach ($attributeMatches as $attributeMatch) {
-                    $element->setAttributeString($attributeMatch['identifier'], trim($attributeMatch['value'], '"'));
-                }
+        
+        $innerHTML = '';
+        $currentTag = array();
+        $element = null;
+        // <html>cont<span>en</span>ts</html>
+        while (strlen($string) > 0) {
+            if (strpos($string, '<') === false) {
+                $elements[] = $this->factory->createText($string);
+                break;
             }
             
-            if (array_key_exists('innerHTML', $match)) {
-                foreach ($this->parse($match['innerHTML']) as $child) {
-                    $element->addChild($child);
+            // find next occurence of <
+            list($buffer, $string) = explode('<', $string, 2);
+            $innerHTML .= $buffer;
+            
+            // see if it is open/close tag
+            if (substr($string, 0, 1) === '/') {
+                // close tag
+                list($tag, $string) = explode('>', substr($string, 1), 2);
+                
+
+                if (count($currentTag) === 0) {
+                    // unexpected end tag
+                    $innerHTML .= '</' . $tag . '>';
+                    continue;
+                } 
+                
+
+                $currentTagIdentifier = array_pop($currentTag);
+                if (count($currentTag) > 0) {
+                    // nested, part of innerHTML
+                    $innerHTML .= '</' . $currentTagIdentifier . '>';
+                } elseif ($tag !== $currentTagIdentifier) {
+                    $innerHTML .= '</' . $tag . ' ' . $currentTagIdentifier . ' b>';
+                    
+                } else {
+                    foreach ($this->parse($innerHTML) as $child) {
+                        $element->addChild($child);
+                    }
+                    $elements[] = $element;
+                    unset($element);
+                    $innerHTML = '';
                 }
+                continue;
+               
+            } 
+
+            // open tag
+            list($tag, $string) = explode('>', $string, 2);
+                          
+            if (strpos($tag, ' ') === false) {
+                $attributesUnparsed = '';
+            } else {
+                list($tag, $attributesUnparsed) = explode(' ', $tag, 2);
             }
             
-            $elements[] = $element;
+            $closedElement = substr($attributesUnparsed, -1) === '/';
+            
+            if ($closedElement) {
+                $attributesUnparsed = substr($attributesUnparsed, 0, -1);
+            } else {
+                $currentTag[] = $tag;
+            }
+
+
+            if (isset($element)) {
+                $innerHTML .= '<' . trim($tag . ' ' . $attributesUnparsed) . ($closedElement ? ' /' : '') . '>';
+                continue;
+            }
+            
+            $element = $this->factory->createElement($tag);
+            
+            // read attributes
+            
+            while (strlen($attributesUnparsed) > 0) {
+                if (strpos($attributesUnparsed, '=') === false) {
+                    break;
+                }
+                
+                list($identifier, $attributesUnparsed) = explode('=', $attributesUnparsed, 2);
+                $identifier = trim($identifier);
+                if (substr($attributesUnparsed, 0, 1) === '"') {
+                    list($value, $attributesUnparsed) = explode('"', substr($attributesUnparsed, 1), 2);
+                } else {
+                    list($value, $attributesUnparsed) = explode(' ', $attributesUnparsed, 2);
+                }
+                
+                $element->setAttributeString($identifier, $value);
+            }
+            
+            if ($closedElement) {
+                $elements[] = $element;
+                unset($element);
+                $innerHTML = '';
+                break;
+            }
         }
+
+        if (count($elements) === 0) {
+            $elements[] = $this->factory->createText($string);
+        }
+        
         return $elements;
     }
 
